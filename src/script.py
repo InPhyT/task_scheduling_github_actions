1618e5bef960bd97a897d2cb8afcfbfc6fdb4814
Claudio Moroni <43729990+ClaudMor@users.noreply.github.com>
2021-12-04 19:50:17 +0100

    fix paths #2
    Co-Authored-By: Pietro Monticone <38562595+pitmonticone@users.noreply.github.com>

--- src/script.py
+++ src/script.py
@@ -5,6 +5,6 @@ import os
 url = "https://covid19.infn.it/iss/csv_part/iss.tar.gz"
 response = requests.get(url, stream=True)
 file = tarfile.open(fileobj=response.raw, mode="r|gz")
-file.extractall(path=os.path.join(os.getcwd(),"0_archive\\"))
+file.extractall(path=os.path.join(os.getcwd(),"0_archive"))
 
 
